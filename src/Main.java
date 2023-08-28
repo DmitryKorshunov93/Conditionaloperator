359e278d56b387aa1d7e2d42f9d3206c8fb53b3b
De93m <DmitryKorshunov93@gmail.com>
2023-08-28 16:56:33 +0300

    Conditional operator

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        int age = 15;
+        if ( age > 18 )
+        {
+            System.out.println("Если возраст человека равен " + age + " то он совершеннолетний");
         }
+        else {
+            System.out.println("Если возраст человека равен " + age + " он не достиг совершеннолетия, нужно немного подождать");
+        }
+
     }
 }
